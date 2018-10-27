f561c8df5151d38ebc23d09833f54c901b7a6188
koichi.sato <stk1.9725@gmail.com>
2018-10-27 11:04:00 +0000

    カラムの追加

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -42,8 +42,12 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+        ]);
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         return redirect('/');
     }
@@ -85,8 +89,12 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+        ]);
         $task = Task::find($id);
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
 
         return redirect('/');
