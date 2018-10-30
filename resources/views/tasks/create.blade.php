9dde89d13d069e0c7737f4a56e6fab2462bca290
koichi.sato <stk1.9725@gmail.com>
2018-10-30 10:32:37 +0000

    課題：Bootstrapのデザイン

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,16 +4,24 @@
 
 <h1>タスク新規作成ページ</h1>
 
+    <div class="row">
+    <div class="col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        <div class="form-group">
+            {!! Form::label('content', 'タスク:') !!}
+            {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
 
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
+        <div class="form-group">
+            {!! Form::label('status', 'ステータス:') !!}
+            {!! Form::text('status', null, ['class' => 'form-control']) !!}
+        </div>
+                
         {!! Form::submit('投稿') !!}
 
     {!! Form::close() !!}
+    </div>
+    </div>
 
 @endsection
